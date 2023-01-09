048965a88113ba45b85798e49dbb703b46a4826a
sulafa-sa1907099 <sa1907099@qu.edu.qa>
2023-01-09 21:21:08 +0300

    Update IdeaCard.js

--- app/ideas/IdeaCard.js
+++ app/ideas/IdeaCard.js
@@ -1,8 +1,7 @@
-import { useEffect } from "react";
+"use client"
 import { CircularProgress } from '@mui/material';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
